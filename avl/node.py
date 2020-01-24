d4b51dacc3d631b333d720ace3675267b0395ff0
Higor Chaves <higor_oliveira@outlook.com>
2020-01-23 22:43:07 -0300

    Add balacing_factor

--- avl/node.py
+++ avl/node.py
@@ -1,8 +1,9 @@
 class Node:
-    def __init__(self, value = None, l_child = None, r_child = None):
+    def __init__(self, value = None, left_child = None, right_child = None):
         self.value = value
-        self.l_child = l_child
-        self.r_child = r_child 
-    
+        self.left_child = left_child
+        self.right_child = right_child 
+        self.balancing_factor = 0
+        
     def __str__(self):
-        return '    {}\n    {}\n    {}'.format(self.value, self.l_child, self.r_child)
+        return '    {}\n    {}\n    {}'.format(self.value, self.left_child, self.right_child)
--- avl/tree.py
+++ avl/tree.py
@@ -13,27 +13,33 @@ def insert_with_node(value, node):
     if node is None:
         return Node(value)
     elif value < node.value:
-        node.l_child = insert_with_node(value, node.l_child)
+        node.left_child = insert_with_node(value, node.left_child)
+        node.balancing_factor -= 1
     elif value > node.value:
-        node.r_child = insert_with_node(value, node.r_child)
+        node.right_child = insert_with_node(value, node.right_child)
+        node.balancing_factor += 1
     return node
-    
-insert(10)
-insert(5)
-insert(3)
-insert(7)
+
+# insert(10)
+# insert(5)
+# insert(3)
+# insert(7)
+# insert(1)
+# insert(4)
+# insert(6)
+# insert(8)
+
+# insert(15)
+# insert(12)
+# insert(17)
+# insert(11)
+# insert(13)
+# insert(16)
+# insert(18)
+
 insert(1)
-insert(4)
-insert(6)
-insert(8)
-
-insert(15)
-insert(12)
-insert(17)
-insert(11)
-insert(13)
-insert(16)
-insert(18)
-
+insert(2)
+insert(3)
+insert(0)
 
 print(root)
