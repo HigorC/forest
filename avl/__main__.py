a93aeb4775ab69369dab9f0f0fa6cc2fa87ba96f
Higor Chaves <higor_oliveira@outlook.com>
2020-02-03 22:54:51 -0300

    Fix balancing factor calculation

---
+++ avl/__main__.py
@@ -0,0 +1,15 @@
+import tree
+import node
+
+
+
+tree.insert(1)
+tree.insert(2)
+tree.insert(3)
+tree.insert(4)
+tree.insert(5)
+
+# inOrdem(root)
+# root = simple_left_rotation(root)
+
+tree.printAvl()
--- avl/tree.py
+++ avl/tree.py
@@ -7,18 +7,57 @@ def insert(value):
     if root is None:
         root = Node(value)
     else:
-        insert_with_node(value, root)
+        root = insert_with_node(value, root)
 
 def insert_with_node(value, node):
     if node is None:
         return Node(value)
     elif value < node.value:
         node.left_child = insert_with_node(value, node.left_child)
-        node.balancing_factor -= 1
     elif value > node.value:
         node.right_child = insert_with_node(value, node.right_child)
-        node.balancing_factor += 1
+
+    return calculate_balancing_factor(node)
+
+def simple_left_rotation(node):
+    aux = node
+    node = node.right_child
+    aux.right_child = node.left_child
+    aux.balancing_factor -=2
+    node.left_child = aux
+    node.balancing_factor -=1
     return node
+
+def calculate_balancing_factor_from_root():
+    global root
+    root = calculate_balancing_factor(root)
+
+def calculate_balancing_factor(node):
+    if node is not None:
+        calculate_balancing_factor(node.left_child)
+        calculate_balancing_factor(node.right_child)
+    
+        if node.left_child is None and node.right_child is None:
+            node.balancing_factor = 0
+        else:
+            left_tree_balance = node.left_child.balancing_factor + 1 if node.left_child is not None else 0
+            right_tree_balance = node.right_child.balancing_factor + 1 if node.right_child is not None else 0
+
+            node.balancing_factor = right_tree_balance - left_tree_balance
+
+            if node.balancing_factor is 2:
+                node = simple_left_rotation(node)
+    return node
+
+def printAvl():
+    print(root)
+
+def inOrdem(node):
+    if node is not None:
+        inOrdem(node.left_child)
+        print (node.value)
+        inOrdem(node.right_child)
+
 
 # insert(10)
 # insert(5)
@@ -37,9 +76,3 @@ def insert_with_node(value, node):
 # insert(16)
 # insert(18)
 
-insert(1)
-insert(2)
-insert(3)
-insert(0)
-
-print(root)
