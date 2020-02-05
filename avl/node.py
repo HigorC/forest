58f537f1e1ac60a60aa4e5ea4ae51c6ce87a47f4
Higor Chaves <higor_oliveira@outlook.com>
2020-02-04 21:51:12 -0300

    Add getHeigth and getBalancingFactor into node

--- avl/node.py
+++ avl/node.py
@@ -7,3 +7,20 @@ class Node:
         
     def __str__(self):
         return '    {}\n    {}\n    {}'.format(self.value, self.left_child, self.right_child)
+
+    def getHeight(self):
+        if self is not None:
+            heightLeftChild = self.left_child.getHeight() if self.left_child is not None else 0
+            heightRightChild = self.right_child.getHeight() if self.right_child is not None else 0
+
+            if self.left_child is None and self.right_child is None:
+                return 1
+
+            return heightLeftChild + 1 if heightLeftChild > heightRightChild else heightRightChild + 1
+        return 0
+
+    def getBalancingFactor(self):
+        heightLeftChild = self.left_child.getHeight() if self.left_child is not None else 0
+        heightRightChild = self.right_child.getHeight() if self.right_child is not None else 0
+
+        return heightRightChild - heightLeftChild
